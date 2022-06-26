426622b0c685194aba706cd9e160bec37e1a625d
stebantalaga <stebantalaga@gmail.com>
2022-06-26 18:08:44 -0500

    subiendo los primeros archivos

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+    else {
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        //do cards match?
+        
+    }
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
